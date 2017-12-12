efe680608426307edeca613c1e48e9bae63fb1ab
Olivier Ramier <olivier.ramier@gmail.com>
2017-12-12 20:11:45 +0100

    [src.base] Base plugin (hello world route)

---
+++ src/base/base.handlers.js
@@ -0,0 +1,6 @@
+/**
+ * Returns hard-coded JSON object
+ */
+exports.helloWorld = async function (request, reply) {
+    return {hello: 'world!'};
+};
---
+++ src/base/base.routes.js
@@ -0,0 +1,14 @@
+const handlers = require('./base.handlers');
+
+exports.register = async function (server, options) {
+    server.route({
+        method: 'GET',
+        path: '/',
+        config: {
+            handler: handlers.helloWorld,
+            description: 'Hello world',
+        }
+    });
+};
+
+exports.name = 'core';
